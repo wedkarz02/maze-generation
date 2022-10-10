3c1955792b77cd541eb80f0c94b69f481ee57c50
Paweł Rybak <pawel.rybak13@gmail.com>
2022-10-10 21:08:39 +0200

    Tested node drawing

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const ctx = canvas.getContext("2d")
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+const padding = 10
+const size = 50
+
+let node = new Node(padding, padding, size)
+node.draw()
