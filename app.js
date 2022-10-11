37d8690e6c3d1394bd307e57b5715eabaa05b5ba
Paweł Rybak <pawel.rybak13@gmail.com>
2022-10-11 19:42:45 +0200

    Added a temporary grid layout

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const nodeSize = 50
 const padding = 10
-const size = 50
 
-let node = new Node(padding, padding, size)
-node.draw()
+const gridWidth = 10
+const gridHeight = 6
+
+let nodes = new Array()
+for (let i = 0; i < gridHeight; i++) {
+    for (let j = 0; j < gridWidth; j++) {
+        let node = new Node(j * (nodeSize + padding), i * (nodeSize + padding), nodeSize)
+        nodes.push(node)
+        node.draw()
+    }
+}
